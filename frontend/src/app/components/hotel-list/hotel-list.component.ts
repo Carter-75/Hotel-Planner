c9f722720e4184886e2829b8725eed4f300c5d38
Carter Moyer <moyer6141@uwlax.edu>
2026-05-01 15:15:38 -0500

    fix

--- frontend/src/app/components/hotel-list/hotel-list.component.ts
+++ frontend/src/app/components/hotel-list/hotel-list.component.ts
@@ -61,6 +61,7 @@ import { HotelCardComponent } from '../hotel-card/hotel-card.component';
     .hotel-item {
       padding: 10px 0;
       height: 200px;
+      width: 100%;
       box-sizing: border-box;
     }
     .empty-msg {
